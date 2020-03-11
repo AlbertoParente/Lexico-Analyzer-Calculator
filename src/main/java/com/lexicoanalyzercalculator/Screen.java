188f2ef52e0f7a78791cf0ffd92d6d27ad8dbea0
AlbertoParente <albertoparente20@gmail.com>
2020-03-11 13:52:44 -0300

    Creating Screen

---
+++ src/main/java/com/lexicoanalyzercalculator/Screen.java
@@ -0,0 +1,14 @@
+package com.lexicoanalyzercalculator;
+
+import javax.swing.JFrame;
+
+public class Screen extends JFrame{
+
+	public static void main(String[] args) {
+		Screen (String title){
+			super (title);
+			setSize(25,200);
+			setVisible(true);
+			}
+}
+}
